7592947f1104576b63e92b86c0ba64dc0f9c9bfb
Adam E <aelderfi@gmail.com>
2019-01-06 10:45:38 +1100

    Create control.R

---
+++ control.R
@@ -0,0 +1,21 @@
+#----------------------------------------------------------------------------
+# Kalman filter examples - control file
+#----------------------------------------------------------------------------
+# This script sets up environment ect..
+
+
+setwd("C:/Users/aelde/OneDrive/Documents/GitHub/Kalman-Filter-Examples")
+
+# Ploting and data import / manipulation
+
+library(tidyverse)
+library(readxl)
+library(dplyr)
+
+# KF packages
+
+library(dlm)
+library(KFAS)
+library(bsts)
+
+
