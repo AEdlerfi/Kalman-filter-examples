f44eb7c8ff331455f4df52574aaa46310b32ce87
Adam E <aelderfi@gmail.com>
2019-01-07 20:15:05 +1100

    Added local linear trend
    Added the local linear trend model. Also set up the DLM outside of the ML estimation, will keep this consistent in all examples as it is clear how the model is built.

--- KF with DLM.R
+++ KF with DLM.R
@@ -44,7 +44,7 @@ KF.tot <- data.frame(
   Filtered.level = dlmFilter(Tot, dlmTot)$f,
   Smoothed.level = dlmSmooth(Tot, dlmTot)$s[-1] 
   )
-                      
+                 
 
 KF.tot %>% 
   mutate(Date = ToT$date) %>% 
@@ -54,9 +54,70 @@ KF.tot %>%
   geom_line(aes(Date, Value, colour = Type))
 
 #-------------------------------------------------------------------------
-# Residual analysis 
+# Local linear trend 
 #-------------------------------------------------------------------------
 
+# tot  = FF0t +vt
+# 0t = GG0t-1 +wt
 
-      
-      
+# FF = [1,0]
+# GG = [1,1  , 0t = [a,b]' # trend 2x2.x.1
+#      0,1]
+# Wt = [0,0
+#       0,1]  # state variance comes from trend
+
+# Construct DLM (need to set out above like this!)
+
+mod2 <- dlm(
+  
+  FF =  matrix(c(1,0), ncol = 2, byrow = TRUE), 
+  
+  V =  matrix(0), 
+  
+  GG = matrix(c(1,1,
+                0,1), ncol = 2, byrow = TRUE) , 
+  
+  W = matrix(c(0,0,
+               0,0), ncol = 2, byrow = TRUE),
+  
+  m0 =  c(1,1), 
+  
+  C0 =  matrix(c(1e+07,0,
+                 0,1e+07), ncol =2,  byrow = TRUE) 
+  
+) 
+
+# Est params
+
+buildFun <- function(params){
+  
+  V(mod2) <- exp(params[1])
+  
+  W(mod2)[2,2] <- exp(params[2])
+  
+  return(mod2)
+  
+}
+
+mod2.est <-  dlmMLE(Tot, parm = c(0,0), build = buildFun)
+
+dlmTotlt <- buildFun(mod2.est$par)
+
+#-------------------------------------------------------------------------
+# Local linear trend 
+#-------------------------------------------------------------------------
+
+KF.tot <- data.frame(
+  Data <- Tot,
+  Filtered.level = dlmFilter(Tot, dlmTotlt)$f,
+  Smoothed.level = dlmSmooth(Tot, dlmTotlt)$s[-1,1] 
+)
+
+
+KF.tot %>% 
+  mutate(Date = ToT$date) %>% 
+  gather(Type, Value, - Date) %>% 
+  filter(Date >= '1960-06-01') %>% 
+  ggplot()+ 
+  geom_line(aes(Date, Value, colour = Type))
+
